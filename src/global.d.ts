938aa5188950ccc9a9d99e46b6b4ee9079a497ca
Phelo Macanda <42580766+PheloMacanda@users.noreply.github.com>
2025-02-11 13:46:35 +0200

    revert changes

--- src/global.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="svelte" />
-
-declare module '*.svelte' {
-    import { SvelteComponent } from 'svelte';
-    const component: typeof SvelteComponent;
-    export default component;
-}
