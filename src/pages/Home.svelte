ae42d6fd46488853e97e70b3a2da31c4e0fbcd69
Phelo Macanda <42580766+PheloMacanda@users.noreply.github.com>
2025-02-11 14:03:46 +0200

    code cleanup

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,14 +1,7 @@
 <script>
     import Post from "../components/Post.svelte";
     import PostInput from "../components/PostInput.svelte";
-
-    let user = {
-        fullName: "John Smith",
-        time: 8,
-        content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        image: "https://images.unsplash.com/photo-1522071820081-009f0129c71c?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVvcGxlJTIwd29ya2luZyUyMG9uJTIwbGFwdG9wfGVufDB8fDB8fHww",
-    };
+    import { user } from "../constants";
 </script>
 
 <main>
