83f235b3ccce0c37f31ff5478e6fe748856abe9c
Phelo Macanda <42580766+PheloMacanda@users.noreply.github.com>
2025-02-11 10:39:51 +0200

    capacitor for android/ios

---
+++ android/app/src/main/java/com/sveltetechassessmentphelo/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sveltetechassessmentphelo.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sveltetechassessmentphelo.app',
+  appName: 'wyzetalk-assessment',
+  webDir: 'dist'
+};
+
+export default config;
