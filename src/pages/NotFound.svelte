86707de85626b80ef88ab1a478d3ecb10d583381
Phelo Macanda <42580766+PheloMacanda@users.noreply.github.com>
2025-02-11 14:01:30 +0200

    routing to other pages

---
+++ src/pages/NotFound.svelte
@@ -0,0 +1,18 @@
+<script lang="ts"></script>
+
+<div class="not-found">
+    <p class="not-found-text">Oops! The page your are looking for is not found!</p>
+</div>
+
+<style>
+    .not-found {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .not-found-text {
+        font-weight: 500;
+        font-size: 16px;
+    }
+</style>
